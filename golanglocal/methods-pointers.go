fce0485e09db50ec527e0c291e7b0b0007f03642
ivens <ivensfernando@gmail.com>
2017-08-25 11:41:49 +0200

    3 i

---
+++ golanglocal/methods-pointers.go
@@ -0,0 +1,22 @@
+package golanglocal
+
+import "fmt"
+
+//type Vertex struct {
+//	X, Y float64
+//}
+
+//func (v Vertex) Abs() float64 {
+//	return math.Sqrt(v.X*v.X + v.Y*v.Y)
+//}
+
+func (v *VertexMethods) Scale(f float64) {
+	v.X = v.X * f
+	v.Y = v.Y * f
+}
+
+func MethodPointer() {
+	v := VertexMethods{3, 4}
+	v.Scale(10)
+	fmt.Println(v.Abs())
+}
